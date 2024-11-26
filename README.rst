7df1de1a7f1aa8dcf0e1769a69fd6a74370bd1c9
Akshay Mestry <xa@mes3.dev>
2024-11-25 22:04:20 -0600

    feat: upstream project to remote
    Signed-off-by: Akshay Mestry <xa@mes3.dev>

---
+++ README.rst
@@ -0,0 +1,6 @@
+.. Author: Akshay Mestry <xa@mes3.dev>
+.. Created on: Monday, November 18 2024
+.. Last updated on: Monday, November 18 2024
+
+xsNumPy
+=======
---
+++ xsnumpy/version.py
@@ -0,0 +1,12 @@
+"""\
+xsNumPy Version
+===============
+
+Author: Akshay Mestry <xa@mes3.dev>
+Created on: Monday, November 18 2024
+Last updated on: Monday, November 18 2024
+
+Module to expose current version details for installed `xsnumpy`.
+"""
+
+version: str = "2024.11.18"
